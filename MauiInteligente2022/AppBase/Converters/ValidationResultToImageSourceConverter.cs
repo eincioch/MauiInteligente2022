a49c92693bf7abb59cebcc38275aeeb3c32ba0b9
jacunar <josueally@gmail.com>
2023-02-03 23:18:07 -0600

    Agregado converter a la página de registro de usuario

---
+++ MauiInteligente2022/AppBase/Converters/ValidationResultToImageSourceConverter.cs
@@ -0,0 +1,21 @@
+﻿using MauiInteligente2022.AppBase.Validations;
+using System.Globalization;
+
+namespace MauiInteligente2022.AppBase.Converters;
+public class ValidationResultToImageSourceConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if(value is ValidationResult validationResult) {
+            return validationResult switch {
+                ValidationResult.None => null,
+                ValidationResult.Valid => ImageSource.FromFile("correct"),
+                ValidationResult.Invalid => ImageSource.FromFile("incorrect"),
+                _ => null
+            };
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
