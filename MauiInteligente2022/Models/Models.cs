6fa2c2fafbc2ea992be289ae7b370320a73a231d
Josue Acuna <josueally@gmail.com>
2023-03-11 13:13:29 -0600

    Agregado nuevas páginas de reporte

--- MauiInteligente2022/Models/Models.cs
+++ MauiInteligente2022/Models/Models.cs
@@ -5,3 +5,7 @@ public record LoginCredentials (string UserName, string Password);
 public record Branch(int BranchId, string Name, string Location) {
     public Branch() : this(0, null!, null!) { }
 }
+
+public record Country(int CountryId, string CountryCode) {
+
+}
