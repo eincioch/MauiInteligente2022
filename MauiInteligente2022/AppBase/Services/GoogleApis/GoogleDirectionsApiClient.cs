9c8594f17f8c26e32a54900e79f61a68661e85e6
jacunar <josueally@gmail.com>
2023-03-19 22:10:05 -0600

    Agregado clases para carga de Google Directions

---
+++ MauiInteligente2022/AppBase/Services/GoogleApis/GoogleDirectionsApiClient.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace MauiInteligente2022.AppBase.Services.GoogleApis; 
+public class GoogleDirectionsApiClient {
+	private readonly HttpClient httpClient;
+	private readonly GoogleDirectionsOptions googleDirectionsOptions;
+
+	public GoogleDirectionsApiClient(HttpClient httpClient,
+				IOptions<GoogleDirectionsOptions> options) {
+		this.httpClient = httpClient;
+		this.googleDirectionsOptions = options.Value;
+	}
+}
---
+++ MauiInteligente2022/AppBase/Services/GoogleApis/GoogleDirectionsOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace MauiInteligente2022.AppBase.Services.GoogleApis; 
+public class GoogleDirectionsOptions {
+    public const string GoogleDirections = "GoogleDirections";
+    public string Url { get; set; }
+    public string ApiKey { get; set; }
+}
