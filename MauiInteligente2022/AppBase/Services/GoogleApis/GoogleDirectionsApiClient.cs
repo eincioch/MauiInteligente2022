65a5cc067b3b8e9799a63fcdf159e2ddbcbeb0ed
jacunar <josueally@gmail.com>
2023-03-21 23:31:44 -0600

    Ajuste cliente de Google Directions

--- MauiInteligente2022/AppBase/Services/GoogleApis/GoogleDirectionsApiClient.cs
+++ MauiInteligente2022/AppBase/Services/GoogleApis/GoogleDirectionsApiClient.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
+using System.Net.Http.Json;
 
 namespace MauiInteligente2022.AppBase.Services.GoogleApis; 
 public class GoogleDirectionsApiClient {
@@ -9,5 +10,18 @@ public class GoogleDirectionsApiClient {
 				IOptions<GoogleDirectionsOptions> options) {
 		this.httpClient = httpClient;
 		this.googleDirectionsOptions = options.Value;
+	}
+
+	public async Task<GoogleDirectionsApiResponse> GetGoogleDirectionsAsync
+		(string origin, string destination) {
+		string uri = string.Format(googleDirectionsOptions.Url, origin, destination, googleDirectionsOptions.ApiKey);
+
+		using var response = await httpClient.GetAsync(uri);
+
+		if (response.IsSuccessStatusCode) {
+			var googleDirectionsResponse = await response.Content.ReadFromJsonAsync<GoogleDirectionsApiResponse>();
+			return googleDirectionsResponse;
+		}
+		return null!;
 	}
 }
